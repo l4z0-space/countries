fa59726b0fff21e82745803bb9eb9cc96ac031dd
l4z0-space <lazaronshyta@gmail.com>
2020-12-15 17:37:28 +0100

    Updates

---
+++ precache-manifest.549fb12527ebca5b4dda346402fa5b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0acfef93e2dd572c5756f9d8d57145a",
+    "url": "/countries/index.html"
+  },
+  {
+    "revision": "756878d5bf5568dfc835",
+    "url": "/countries/static/css/main.e2928a78.chunk.css"
+  },
+  {
+    "revision": "93107e005e4fe627d24a",
+    "url": "/countries/static/js/2.b50d39b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countries/static/js/2.b50d39b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "756878d5bf5568dfc835",
+    "url": "/countries/static/js/main.f9b47b16.chunk.js"
+  },
+  {
+    "revision": "b73eae8a0df83cdc2337",
+    "url": "/countries/static/js/runtime-main.f7e05aad.js"
+  }
+]);
